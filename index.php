a1a7959a74f580fde5598002fa6d9bd47c5f3128
albertoferri <falberto99@gmail.com>
2024-04-02 15:43:10 +0100

    aggiunta della funzione costrutture, creato array di film e la funzione che mi restituisce la stringa, problema di stampa in pagina

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
+// creo la classe movie con le proprietà che dovrà avere ogni oggetto
 class Movie {
     public $title;
     public $director;
     public $year;
 
+// aggiungo una funzione costruttore per "montare" l'oggetto con le proprietà
+    function __construct($_title, $_director, $_year) {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->year = $_year;
+    }
+
+    public function getMovieDetails() {
+        return $this->title . ', diretto da ' . $this->director . ', uscito nel ' . $this->year;
+    }
 }
+// creo un array di oggetti film
+$movies = [
+    $movie1,
+    $movie2
+];
+
+// aggiungo qui i film
+$movie1 = new Movie("Harry Potter e la pietra filosofale", "Chris Columbus", 2001);
+$movie2 = new Movie("Harry Potter e la camera dei segreti", "Chris Columbus", 2002);
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +45,11 @@ class Movie {
         <div class="row my-5">
             <h1 class="col-12">PHP-oop-1</h1>
             <ul class="col">
-                
+            <?php
+            foreach($movies as $movie) {
+                echo "<li>" . $movie->getMovieDetails() . "</li>";
+            }
+            ?>
             </ul>
         </div>
     </div>
